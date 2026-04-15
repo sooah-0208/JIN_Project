3ad201d14483996c73ed7bd5be65dda2f9375e32
sooah-0208 <santacoyaki11@gmail.com>
2026-04-15 11:45:25 +0900

    리엑트라우터

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,34 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router";
+
+const Home = () => {
+  return (
+    <div className="text-center">
+      <h1>메인 화면입니다.</h1>
+    </div>
+  )
+}
+
+const NotFound = () => {
+  return (
+    <div className="text-center">
+      <h1>404</h1>
+      <p>페이지를 찾을 수 없습니다.</p>
+    </div>
+  )
+}
 
 const App = () => {
-
+  const paths = [
+    {path: "/", element: <Home />},
+    {path: "*", element: <NotFound />},
+  ]
   return (
     <>
-      
+      <BrowserRouter>
+        <Routes>
+          { paths?.map((v, i) => <Route key={i} path={v.path} element={v.element} />) }
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
